58c8199aedd89d9edda0cca6b04af314f4bf6b3e
ikamrava <ikamrava>
2023-02-03 17:21:04 +0000

    update project links

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,6 +7,7 @@ import news from "../public/assets/projects/news.png"
 import book from "../public/assets/projects/book.png"
 import quiez from "../public/assets/projects/quiez.png"
 import pass from "../public/assets/projects/pass.png"
+import weather from "../public/assets/projects/weather.png"
 
 
 
@@ -19,11 +20,12 @@ export const Projects = () => {
             <p className=' uppercase text-xl tracking-widest text-[#5E17EB]'>ProJects</p>
             <h2 className=' py-4 '>What I've Built</h2>
             <div className=' grid md:grid-cols-2 gap-8'>
-                <ProjectItem imagelink={hulu} title="Raect" link="/" basedon="React"/>
-                <ProjectItem imagelink={news} title="Raect" link="/" basedon="React" />
+                <ProjectItem imagelink={hulu} title="Raect" link="https://hulu-video.ikamdev.co.uk/" basedon="React"/>
+                <ProjectItem imagelink={news} title="Raect" link="https://vercel.com/ikamrava/latest-news" basedon="React" />
                 <ProjectItem imagelink={book} title="Raect" link="https://top-seller-books.ikamdev.co.uk/" basedon="React"/>
-                <ProjectItem imagelink={quiez} title="Raect" link="/" basedon="React"/>
-                <ProjectItem imagelink={pass} title="Raect" link="/" basedon="Chrome Extension"/>
+                <ProjectItem imagelink={quiez} title="Raect" link="http://quizzical-app.ikamdev.co.uk/" basedon="React"/>
+                <ProjectItem imagelink={weather} title="Raect" link="https://weather-app.ikamdev.co.uk/" basedon="React"/>
+                <ProjectItem imagelink={pass} title="Raect" link="https://chrome.google.com/webstore/detail/password-generator/oaoomholmcdkngngnfnkiogolifgddca" basedon="Chrome Extension"/>
             </div>
         </div>
 
